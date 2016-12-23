5fed0acefb2abb33873f52397ad79a58329da429
EastL <st100321048@gmail.com>
2016-12-22 22:58:41 -0800

    add test socks client

--- socks_server/socks.c
+++ socks_server/socks.c
@@ -13,8 +13,27 @@ int socks_request(int sfd)
 {
 	char buf[1024];
 	memset(buf, 0, 1024);
-	read(sfd, buf, 1024);	
-	printf("%s\n", buf);
+	if (read(sfd, buf, 1024) < 0)
+		return SER_ERR;
+
+	for (int i = 0; i < 20; i++)
+		printf("%x\n", buf[i]);
+
+	socks_t *temp = malloc(sizeof(socks_t));
+	temp->vn = buf[0];
+	temp->cd = buf[1];
+	temp->port = buf[2] * 256 + buf[3];
+	
+	char IP[20];
+	memset(IP, 0, 20);
+
+	sprintf(IP, "%d.%d.%d.%d", buf[4], buf[5], buf[6], buf[7]);
+	strcpy(temp->ip, IP);
+
+	strcpy(temp->user_id, (char*)&buf[9]);
+
+	printf("VN: %d, CD: %d, DST IP: %s, DST PORT: %d, USERID: %s\n", temp->vn, temp->cd, temp->ip, temp->port, temp->user_id);
+	
 	return 0;
 }
 
